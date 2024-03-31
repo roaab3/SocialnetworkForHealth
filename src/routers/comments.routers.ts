7df4c27493ddc3ccf2f53c14f90ba2cb4442491f
roaabader3 <128834629+roaabader3@users.noreply.github.com>
2024-03-31 10:15:11 +0300

    Delete src directory

--- src/routers/comments.routers.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { CommentsController } from "../controllers/commentController";
-
-const router = Router();
-router.post("/createComment", CommentsController.createComment);
-router.delete("/deleteComment", CommentsController.deleteComment);
-
-export default router;
