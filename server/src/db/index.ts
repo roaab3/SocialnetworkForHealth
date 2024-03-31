7033502c57a9ffd70b6f849727b2951b917636ea
roaabader3 <128834629+roaabader3@users.noreply.github.com>
2024-03-31 10:47:01 +0300

    Update index.ts

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,9 +4,6 @@ mongoose.set("strictQuery", true);
 
 const connectDb = async () => {
    await mongoose.connect("mongodb://localhost:27017/parpar-server");
-//   await mongoose.connect(
-//     "mongodb+srv://Tareez:ghandour@test.dey18uy.mongodb.net/?retryWrites=true&w=majority"
-//   );
 
 const posts = mongoose.model("posts");
 const indexes = await posts.collection.indexes();
